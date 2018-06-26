9344d45775f0fa603d8b467765c9755787a3625a
Dorian Holmes <restlessd@fb.com>
2018-06-26 13:14:27 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Dorian Holmes on 6/26/18.
+//  Copyright © 2018 Dorian Holmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Dorian Holmes on 6/26/18.
+//  Copyright © 2018 Dorian Holmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
